67bb5f654c7fda6e196a1b8219be048ebc5bab8c
sharad <sharadbhosale567@gmail.com>
2022-05-03 17:26:46 +0530

    creat contact details

--- src/com/Day24Problems/Main.java
+++ src/com/Day24Problems/Main.java
@@ -2,6 +2,16 @@ package com.Day24Problems;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("welcome to java Stream Problems");
+
+        PersonDetails person = new PersonDetails();
+        person.firstName("Sharad");
+        person.lastName("Bhosale");
+        person.Address("At post ");
+        person.City("nashik");
+        person.State("Maharashtra");
+        person.ZipCode(420000);
+        person.PhoneNumber("7387232500");
+        person.Email("sgb@gmail.com");
+        System.out.println(person.toString());
     }
 }
