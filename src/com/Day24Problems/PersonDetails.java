c85cdd86c759c50cff89de789d6ad681b31a6249
sharad <sharadbhosale567@gmail.com>
2022-05-03 17:37:29 +0530

    Edit contacts

--- src/com/Day24Problems/PersonDetails.java
+++ src/com/Day24Problems/PersonDetails.java
@@ -10,6 +10,18 @@ public class PersonDetails {
     private String phoneNumber;
     private String email;
 
+    public PersonDetails(String firstName, String lastName, String address, String city, String state,
+                         String zipCode, String phoneNumber,  String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
     public String getFirstName() {
         return firstName;
     }
